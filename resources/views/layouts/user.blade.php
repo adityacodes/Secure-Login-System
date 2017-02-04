398434fd246df536fa57249c523eaabd55aa758d
adityacodes <adityapadhi1996@gmail.com>
2017-02-04 16:15:35 +0530

    Finished dashboard part of user panel moving to controller and route

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -2,128 +2,173 @@
 <html>
 <head>
 	<title>@yield('title') | MMM </title>
+
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
 	<link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('css/font-awesome.min.css')}}">
-	<style type="text/css">
-		
-		.panel-default>.panel-heading
-		{
-			background-color: #FEF8E4;
-			border-color: #FEEEBD;
-		}
+	<link rel="stylesheet" type="text/css" href="{{asset('css/jquery-ui.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
 
-		.body, .panel-default>.panel-body
-		{
-			background-color: #FEEEBD;
-		}
+	@yield('stylesheets')
 
-		.panel-default{
-
-			border-color: #FEEEBD;
-		}
-
-
-		.bttn{
-			font-size: 12px;
-			background-color: #FEEEBD;
-		}
-		.bttn:hover ,
-		.bttn:focus,
-		.bttn:active{
-			border: 0.1px solid red;
-			background-color: #FEE890;
-		}
-
-	</style>
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
-<body class="body">
+	<body class="body">
 
-<div class="container-fluid" style="padding: 0;">
-	<div class="panel panel-default">
-	    <div class="panel-heading">
-	    	<ul class="list-inline">
+		<div class="container-fluid" style="padding: 0;">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<ul class="list-inline">
 
-		    	<li>
-		    		<img src="http://placehold.it/61x40">
-		    	</li>
-	    		<li>
-	    			<a style="color: black;" href="{{url('/dashboard')}}"><button class="btn bttn"><img width="16" height="16" src="{{asset('icons/desktop.png')}}"> DASHBOARD</button></a>
-	    		</li>
+						<li>
+							<img src="http://placehold.it/61x40">
+						</li>
+						<li>
+							<a style="color: black;" href="{{url('/dashboard')}}"><button class="btn bttn"><img width="16" height="16" src="{{asset('icons/desktop.png')}}"> DASHBOARD</button></a>
+						</li>
 
-	    		<li>
-	    			<div class="dropdown">
-					    <button class="btn bttn dropdown-toggle" type="button" data-toggle="dropdown">
-					    	<img width="16" height="16" src="{{asset('icons/groupicon.png')}}"> PARTICIPANTS
-					    <span class="caret"></span></button>
-					    <ul class="dropdown-menu">
-					      <li><a href="#">TRUSTEE</a></li>
-					      <li><a href="#">REFERALS</a></li>
-					    </ul>
-					</div>
-	    		</li>
+						<li>
+							<div class="dropdown">
+								<button class="btn bttn dropdown-toggle" type="button" data-toggle="dropdown">
+									<img width="16" height="16" src="{{asset('icons/groupicon.png')}}"> PARTICIPANTS
+									<span class="caret"></span>
+								</button>
+								<ul class="dropdown-menu">
+									<li>
+										<a id="trustee" href="" title="Participants that appointed you as a Trustee (If you have been so:-)) Details on the page 'HOW DOES IT WORK', section 'Trustees'. (TO Get to know who are trustees "> 
+											<img width="16" height="16" src="{{asset('icons/groupicon.png')}}">TRUSTEE
+										</a>
+									</li>
+									<li>
+										<a id="referals" href="" title="You receive Referral Bonus for your referrals.">
+											<img width="16" height="16" src="{{asset('icons/groupicon.png')}}">REFERALS
+										</a>
+									</li>
+								</ul>
+							</div>
+						</li>
 
-	    		<li>
-	    			<div class="dropdown">
-					    <button class="btn bttn dropdown-toggle" type="button" data-toggle="dropdown"> <img width="16" height="16" src="{{asset('icons/user.png')}}"> MY PAGE
-					    <span class="caret"></span></button>
-					    <ul class="dropdown-menu">
-					      <li><a href="#"> <img width="16" height="16" src="{{asset('icons/user.png')}}"> PROFILE</a></li>
-					      <li><a href="#">MY LETTER OF HAPPINESS</a></li>
-					    </ul>
-					</div>
-				</li>
+						<li>
+							<div class="dropdown">
+								<button class="btn bttn dropdown-toggle" type="button" data-toggle="dropdown"> 
+									<img width="16" height="16" src="{{asset('icons/user.png')}}"> 
+									MY PAGE
+									<span class="caret"></span>
+								</button>
+								<ul class="dropdown-menu">
+									<li>
+										<a href="{{ url('/profile') }}" title="Information you submitted during registration"> <img width="16" height="16" src="{{asset('icons/user.png')}}"> 
+											PROFILE
+										</a>
+									</li>
+									<li>
+										<a href="{{ url('/letter-of-happiness') }}"> 
+											<img width="16" height="16" src="{{asset('icons/letter.png')}}"> 
+											MY LETTER OF HAPPINESS
+										</a>
+									</li>
+								</ul>
+							</div>
+						</li>
 
-	    		<li>
-	    			<button class="btn bttn"> <img width="16" height="16" src="{{asset('icons/promo.png')}}"> PROMO TASKS</button>
-	    		</li>
+						<li>
+							<a href="{{ url('/promo-tasks') }}">
+								<button class="btn bttn"> 
+									<img width="16" height="16" src="{{asset('icons/promo.png')}}"> PROMO TASKS
+								</button> 
+							</a>
+						</li>
 
-	    		<li>
-	    			<button class="btn bttn"> MAVRO</button>
-	    		</li>
+						<li>
+							<button class="btn bttn"> MAVRO</button>
+						</li>
 
-	    		<li>
-	    			<button class="btn bttn"> <img width="16" height="16" src="{{asset('icons/account.png')}}"> ACCOUNTS</button>
-	    		</li>
+						<li>
+							<a href="{{ url('/accounts') }}">
+								<button class="btn bttn"> <img width="16" height="16" src="{{asset('icons/account.png')}}"> ACCOUNTS
+								</button>
+							</a>
+						</li>
 
-	    		
 
-	    		<li class="pull-right">
-	    			<button class="btn bttn"><img width="16" height="16" src="{{asset('icons/logout.png')}}"> 
-	    			LOGOUT</button>
-	    		</li>
 
-	    		<li class="pull-right">
-	    			<button class="btn bttn"><img width="16" height="16" src="{{asset('icons/support.png')}}"> 
-	    			SUPPORT</button>
-	    		</li>
+						<li class="pull-right">
+						<a href="{{url('logout')}}">
+							<button class="btn bttn"><img width="16" height="16" src="{{asset('icons/logout.png')}}"> 
+								LOGOUT
+							</button>
+						</a>
+						</li>
 
-	    		<li class="pull-right">
-	    			<div class="dropdown">
-					    <button class="btn bttn dropdown-toggle" type="button" data-toggle="dropdown"> <img width="16" height="16" src="{{asset('icons/information.png')}}"> Information
-					    <span class="caret"></span></button>
-					    <ul class="dropdown-menu">
-					      <li><a href="#"> NEWS</a></li>
-					      <li><a href="#"> FAQs </a></li>
-					      <li><a href="#"> Promotion </a></li>
-					      <li><a href="#"> Webnairs </a></li>
-					    </ul>
-					</div>
-				</li>
+						<li class="pull-right">
+							<a href="{{ url('/support') }}">
+								<button class="btn bttn"><img width="16" height="16" src="{{asset('icons/support.png')}}"> 
+									SUPPORT
+								</button>
+							</a>
+						</li>
 
-				<li class="pull-right">
-	    			<button class="btn bttn"><img width="16" height="16" src="{{asset('icons/inbox.png')}}"><sup style="color: red;">0</sup></button>
-	    		</li>
+						<li class="pull-right">
+							<div class="dropdown">
+								<button class="btn bttn dropdown-toggle" type="button" data-toggle="dropdown"> 
+									<img width="16" height="16" src="{{asset('icons/information.png')}}"> Information
+									<span class="caret"></span>
+								</button>
+									<ul class="dropdown-menu">
+										<li>
+											<a href="{{url('/news')}}"> 
+												<img width="16" height="16" src="{{asset('icons/news.png')}}"> NEWS
+											</a>
+										</li>
+										<li>
+											<a href="{{url('/faq')}}"> 
+												<img width="16" height="16" src="{{asset('icons/faq.png')}}"> FAQs 
+											</a>
+										</li>
+										<li>
+											<a href="{{url('/promotion')}}">
+												<img width="16" height="16" src="{{asset('icons/promotion.png')}}"> Promotion 
+											</a>
+										</li>
+										<li>
+											<a href="{{url('/webnairs')}}"><img width="16" height="16" src="{{asset('icons/information.png')}}"> 
+												Webnairs 
+											</a>
+										</li>
+									</ul>
+							</div>
+						</li>
 
-	    	</ul>
+						<li class="pull-right">
+							<a href="{{url('/notices')}}">
+								<button class="btn bttn">
+									<img width="16" height="16" src="{{asset('icons/inbox.png')}}">
+									<sup style="color: red;">0</sup>
+								</button>
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div class="panel-body">
+					@yield('content')
+				</div>
+			</div>
+		</div>
+		<script type="text/javascript" src="{{asset('js/jquery.js')}}"></script>
+		<script type="text/javascript" src="{{asset('js/bootstrap.js')}}"></script>
+		<script type="text/javascript" src="{{asset('js/jquery-ui.js')}}"></script>
+		<script type="text/javascript" src="{{asset('js/app.js')}}"></script>
 
-	    </div>
-	    <div class="panel-body">
-	    					
-	    @yield('content')
-	    </div>
-  	</div>
-</div>
-	<script type="text/javascript" src="{{asset('js/jquery.js')}}"></script>
-	<script type="text/javascript" src="{{asset('js/bootstrap.js')}}"></script>
-</body>
+	</body>
 </html>
--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -5,34 +5,61 @@
 
 @section('stylesheets')
 	<style type="text/css">
-			
+	.span10{
+		margin-top: 20px;
+	}
+	a:hover{
+		text-decoration: none;
+	}
 	</style>
-	
+
 
 @endsection
 
 @section('content')
 
-<div class="container">    			
-	<div class="alert alert-info text-center">
-		 <strong><a href="{{url('/warning')}}" class="alert-link">WARNING!</a></strong> <span style="color: red;">THIS IS A COMMUNITY OF MUTUAL FINANCIAL HELP!</span> Participate only with spare money. Don't contribute all the money you have.
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12 alert alert-info text-center">
+			<strong>
+				<a href="{{url('/warning')}}" class="alert-link">WARNING!</a>
+			</strong> 
+			<span style="color: red;">THIS IS A COMMUNITY OF MUTUAL FINANCIAL HELP!</span> 
+			Participate only with spare money. Don't contribute all the money you have.
+		</div>	
+
+	</div> 
+
+	<div class="row">
+		<div class="col-md-12">
+			<img class="img-center img-responsive" style="width: 100%;" src="{{asset('/image/image1.png')}}">
+		</div>
+		<div class="col-md-12 span10">
+			<img class="img-center img-responsive" style="width: 100%;" src="{{asset('/image/mavro.png')}}">
+		</div>
 	</div>
 
-	<div class="row text-center">
-		<img class="img-center" src="{{asset('/image/image1.png')}}">
-	</div>
+	<hr>
 
-	<div class="row"><br><br>
-		<div class="col-md-6 text-left">
-			<button class="btn btn-lg btn-block btn-success"><h1>Provide Help</h1>
-			  Acquire Mavro<em>(make contribution)</em>
-			</button>
+	<div class="row">
+		<div class="col-md-12">
+				<div class="col-md-6 col-xs-12">
+					<a href="{{ url('/provide-help') }}">
+						<button class="btn btn-lg btn-block btn-success"><h1>Provide Help</h1>
+							Acquire Mavro<em>(make contribution)</em>
+						</button>
+					</a>
+				</div>
+				<div class="col-md-6 col-xs-12">
+					<a href="{{ url('/get-help') }}">
+						<button class="btn btn-lg btn-block btn-info "><h1>Get Help</h1>
+							"Cash-out" Mavro
+						</button>
+					</a>
+				</div>
+
 		</div>
-		<div class="col-md-6 text-left">
-			<button class="btn btn-lg btn-block btn-info "><h1>Get Help</h1>
-				"Cash-out" Mavro
-			</button>
-		</div>
+		
 	</div>
 </div>
 
