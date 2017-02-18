3c0be2dcd2bc5eb2237571e0f2f7047930cb89d6
siddas27 <siddharthdas8@gmail.com>
2017-02-18 19:10:02 +0530

    Changes to accounts and git ignore

--- resources/views/users/main/accounts.blade.php
+++ resources/views/users/main/accounts.blade.php
@@ -1,1 +1,37 @@
 @extends('layouts.user')
+
+
+@section('title', 'My Profile')
+
+@section('stylesheets')
+
+
+
+
+@endsection
+
+
+@section('content')
+<div class="panel-body">
+
+sadasdasdasdsadasdad
+
+
+
+
+
+
+
+</div>
+
+@endsection
+
+
+
+@section('scripts')
+
+
+
+
+
+@endsection
