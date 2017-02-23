19fc1f08f595ae66bb68285f28c636c749122190
adityacodes <adityapadhi1996@gmail.com>
2017-02-23 17:00:47 +0530

    Login check for mail confirmation added

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Auth, Session;
+class LoginController extends Controller
+{
+    public function postLogin(Request $request)
+	  {
+	    
+	      $this->validate($request, array(
+				'email' => 'required|exists:users',
+	          	'password' => 'required'
+			));
+	 
+	      $credentials = [
+	          'email' => $request->email,
+	          'password' => $request->password,
+	          'confirmed' => 1
+	      ];
+	 
+	      if ( ! Auth::attempt($credentials))
+	      {
+	        Session::flash('warning', 'Username or password incorrect.');
+	        return Redirect::to('login');
+	      }
+	      Session::flash('success', 'You have successfully logged in.!');
+	      return redirect()->intended('dashboard');
+
+	  }
+}
