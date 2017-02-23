400be8f1c3c54ba0022c6daac8810ca2cf21a482
adityacodes <adityapadhi1996@gmail.com>
2017-02-23 16:27:27 +0530

    login view modified

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -89,7 +89,7 @@
 
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <button type="submit" class="btn btn-primary" style="background: darkorange;">
                                      Login
                                     </button>
 
