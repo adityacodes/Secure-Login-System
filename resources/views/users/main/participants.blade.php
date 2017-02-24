5439697f8b06b88a4dc54a2b0f829775345b32de
siddas27 <siddharthdas8@gmail.com>
2017-02-24 15:20:56 +0530

    easyui added

--- resources/views/users/main/participants.blade.php
+++ resources/views/users/main/participants.blade.php
@@ -4,14 +4,6 @@
 
 @section('stylesheets')
 
-    <style>
-
-        .border{
-            border-width: 2px;
-            border-color: #2b2b2b;
-        }
-
-    </style>
 
 
 
@@ -19,25 +11,28 @@
 
 
 @section('content')
-    <div id="slide-panel">
-        <a href="#" class="btn btn-danger opener" id="opener"><i class="glyphicon glyphicon-align-justify"></i></a>
-        <div id="panels" class="panel panel-default panel2">
-            <div class="ph border panel-body ">
-                <h5>My participants</h5>
-
-            </div>
-            <div class="panel-body">
-
-            </div>
+    <div style="margin:20px 0;"></div>
+    <div class="easyui-layout" style="width:700px;height:350px;">
+        <div data-options="region:'north'" style="height:50px"></div>
+        <div data-options="region:'south',split:true" style="height:50px;"></div>
+        <div data-options="region:'east',split:true" title="East" style="width:100px;"></div>
+        <div data-options="region:'west',split:true" title="West" style="width:100px;"></div>
+        <div data-options="region:'center',title:'Main Title',iconCls:'icon-ok'">
+            <table class="easyui-datagrid"
+                   data-options="url:'datagrid_data1.json',method:'get',border:false,singleSelect:true,fit:true,fitColumns:true">
+                <thead>
+                <tr>
+                    <th data-options="field:'itemid'" width="80">Item ID</th>
+                    <th data-options="field:'productid'" width="100">Product ID</th>
+                    <th data-options="field:'listprice',align:'right'" width="80">List Price</th>
+                    <th data-options="field:'unitcost',align:'right'" width="80">Unit Cost</th>
+                    <th data-options="field:'attr1'" width="150">Attribute</th>
+                    <th data-options="field:'status',align:'center'" width="60">Status</th>
+                </tr>
+                </thead>
+            </table>
         </div>
     </div>
-    <article id="content">
-        <div class="panel panel-default panel1">
-            <div class="panel-body ph">
-                <
-            </div>
-        </div>
-    </article>
 @endsection
 
 
@@ -45,45 +40,6 @@
 @section('scripts')
     <script src="//cdnjs.cloudflare.com/ajax/libs/x-editable/1.5.0/jqueryui-editable/js/jqueryui-editable.min.js"></script>
 
-    <script>
-        $(document).ready(function(){
-            $('#opener,#add,#edit').click(function() {
-                var panel = $('#slide-panel');
-                if (panel.hasClass("visible")) {
-                    panel.removeClass('visible').animate({'margin-left':'-500px'});
-                    $('#content').css({'margin-right':'0px'});
-                } else {panel.addClass('visible').animate({'margin-left':'0px'});
-                    $('#content').css({'margin-right':'-500px'});
-                }
-                return false;
-            });
-        });
-        $(function () {
-            $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
-            $('.tree li.parent_li > span').on('click', function (e) {
-                var children = $(this).parent('li.parent_li').find(' > ul > li');
-                if (children.is(":visible")) {
-                    children.hide('fast');
-                    $(this).attr('title', 'Expand this branch').find(' > i').addClass('fa-plus').removeClass('fa-minus');
-                } else {
-                    children.show('fast');
-                    $(this).attr('title', 'Collapse this branch').find(' > i').addClass('fa-minus').removeClass('fa-plus');
-                }
-                e.stopPropagation();
-            });
-
-        });
-        $.fn.editable.defaults.mode = 'inline';
-        $('.name').editable({
-            type: 'text',
-            pk: 1,
-            url: '/post',
-            title: 'Enter username'
-
-        });
-        $('#dt').editable();
-
-    </script>
 
 
 
