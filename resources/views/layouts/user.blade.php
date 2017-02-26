12e0f996c02a15425d10b93ba041da728859f654
adityacodes <adityapadhi1996@gmail.com>
2017-02-26 16:00:03 +0530

    Ädded loader to user.blade

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -8,176 +8,17 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="">
 	<meta name="author" content="">
-
-	{{-- <link rel="stylesheet" type="text/css" href="{{asset('css/styler.css')}}"> --}}
 	<link rel="stylesheet" type="text/css" href="{{asset('easyui/themes/default/easyui.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('easyui/themes/icon.css')}}">
 	<link rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}">
-	{{-- <link rel="stylesheet" type="text/css" href="{{asset('easyui/demo/demo.css')}}"> --}}
 	<link href="{{ captcha_layout_stylesheet_url() }}" type="text/css" rel="stylesheet">
 	@yield('stylesheets')
 
 </head>
 
 	<body class="easyui-layout bg" id="layoutcc">
+	<div class="loader" style="background: url({{asset('loader.gif')}}) 50% 50% no-repeat #404040;"></div>
 
-
-
-		{{-- <div class="container-fluid" style="padding: 0;">
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<ul class="list-inline newnav">
-
-						<li>
-							<img width="61" height="40" src="{{asset('icons/mmm.png')}}">
-						</li>
-						<li class="{{Request::is('dashboard')? 'active' : ''}}">
-							<a style="color: black;" href="{{url('/dashboard')}}">
-								<button class="btn bttn">
-									<img width="16" height="16" src="{{asset('icons/desktop.png')}}"> 
-									Dashboard
-								</button>
-							</a>
-						</li>
-
-						<li class="{{Request::is('participants') ||
-									  Request::is('referals') ? 'active' : ''}}">
-							<div class="dropdown">
-								<button class="btn bttn dropdown-toggle" type="button" data-toggle="dropdown">
-									<img width="16" height="16" src="{{asset('icons/groupicon.png')}}"> Participants
-									<span class="caret"></span>
-								</button>
-								<ul class="dropdown-menu">
-									<li>
-										<a id="participants" href="{{url('/participants')}}" title="Know about all the participants here. Have the ability to add them. ">
-											<img width="16" height="16" src="{{asset('icons/groupicon.png')}}">Participants
-										</a>
-									</li>
-									<li>
-										<a id="referals" href="{{url('/referals')}}" title="You receive Referral Bonus for your referrals.">
-											<img width="16" height="16" src="{{asset('icons/groupicon.png')}}">Referals
-										</a>
-									</li>
-								</ul>
-							</div>
-						</li>
-
-						<li class="{{Request::is('profile') || 
-									  Request::is('letter-of-happiness') ? 'active' : ''}}
-
-									">
-							<div class="dropdown">
-								<button class="btn bttn dropdown-toggle" type="button" data-toggle="dropdown"> 
-									<img width="16" height="16" src="{{asset('icons/user.png')}}"> 
-									My Page
-									<span class="caret"></span>
-								</button>
-								<ul class="dropdown-menu">
-									<li>
-										<a href="{{ url('/profile') }}" title="Information you submitted during registration"> <img width="16" height="16" src="{{asset('icons/user.png')}}"> 
-											Profile
-										</a>
-									</li>
-									<li>
-										<a href="{{ url('/letter-of-happiness') }}"> 
-											<img width="16" height="16" src="{{asset('icons/letter.png')}}"> 
-											My Letter Of Happiness
-										</a>
-									</li>
-								</ul>
-							</div>
-						</li>
-
-						<li class="{{Request::is('promo-tasks')? 'active' : ''}}">
-							<a href="{{ url('/promo-tasks') }}">
-								<button class="btn bttn"> 
-									<img width="16" height="16" src="{{asset('icons/promo.png')}}"> Promo Tasks
-								</button> 
-							</a>
-						</li>
-
-						<li class="{{Request::is('mavro')? 'active' : ''}}">
-							<button class="btn bttn"> Mavro</button>
-						</li>
-
-						<li class="{{Request::is('accounts')? 'active' : ''}}">
-							<a href="{{ url('/accounts') }}">
-								<button class="btn bttn"> <img width="16" height="16" src="{{asset('icons/account.png')}}"> Accounts
-								</button>
-							</a>
-						</li>
-
-
-
-						<li class="pull-right">
-						<a href="{{url('logout')}}">
-							<button class="btn bttn"><img width="16" height="16" src="{{asset('icons/logout.png')}}"> 
-								Logout
-							</button>
-						</a>
-						</li>
-
-						<li class="pull-right {{Request::is('support')? 'active' : ''}}">
-							<a href="{{ url('/support') }}">
-								<button class="btn bttn"><img width="16" height="16" src="{{asset('icons/support.png')}}"> 
-									Support
-								</button>
-							</a>
-						</li>
-
-						<li class="pull-right
-									{{Request::is('news') || 
-									  Request::is('faq') || 
-									  Request::is('promotion-materials') || 
-									  Request::is('webnairs') ? 'active' : ''}}
-
-									">
-							<div class="dropdown">
-								<button class="btn bttn dropdown-toggle" type="button" data-toggle="dropdown"> 
-									<img width="16" height="16" src="{{asset('icons/information.png')}}"> Information
-									<span class="caret"></span>
-								</button>
-									<ul class="dropdown-menu">
-										<li>
-											<a href=""> 
-												<img width="16" height="16" src="{{asset('icons/news.png')}}"> News
-											</a>
-										</li>
-										<li>
-											<a href="{{url('/faq')}}"> 
-												<img width="16" height="16" src="{{asset('icons/faq.png')}}"> FAQs 
-											</a>
-										</li>
-										<li>
-											<a href="{{url('/promotion-materials')}}">
-												<img width="16" height="16" src="{{asset('icons/promotion.png')}}"> Promotion 
-											</a>
-										</li>
-										<li>
-											<a href="{{url('/webnairs')}}"><img width="16" height="16" src="{{asset('icons/information.png')}}"> 
-												Webnairs 
-											</a>
-										</li>
-									</ul>
-							</div>
-						</li>
-
-						<li class="pull-right {{Request::is('notices')? 'active' : ''}}">
-							<a href="{{url('/notices')}}">
-								<button class="btn bttn">
-									<img width="16" height="16" src="{{asset('icons/inbox.png')}}">
-									<sup style="color: red;">0</sup>
-								</button>
-							</a>
-						</li>
-					</ul>
-				</div>
-
-				</div>
-			</div>
-		</div> 
-
-		--}}
 		<div data-options="region:'north'" style="margin-top: -2px; height: 52px; overflow: hidden;">
 			<div style="border-bottom: solid 3px #8E846B;">
 				<div class="easyui-panel" style="background-color: #FEF8E4; color: #3F0D13; padding-top: 10px; border: 0px;">
@@ -235,7 +76,9 @@
 
 	<script type="text/javascript" src="{{ asset('easyui/jquery.min.js') }}"></script>
 	<script type="text/javascript" src="{{asset('easyui/jquery.easyui.min.js')}}"></script>
-	<script type="text/javascript" src="{{asset('js/extendjquery.js')}}"></script>
+	<script type="text/javascript">
+		$('.loader').fadeOut('slow');
+	</script>
 	<script type="text/javascript" src="{{asset('easyui/jquery.formtowizard.js')}}"></script>
 	<script type="text/javascript" src="{{asset('easyui/easyloader.js')}}"></script>
 	@yield('scripts')
--- resources/views/users/account/index.blade.php
+++ resources/views/users/account/index.blade.php
@@ -5,79 +5,44 @@
 
 @section('stylesheets')
 
-    <style>
-        #header { text-align:center; border-bottom:solid 1px #b2b3b5; margin: 0 0 20px 0; }
-        fieldset { border:none; width:320px;}
-        legend { font-size:18px; margin:0px; padding:10px 0px; color:#b0232a; font-weight:bold;}
-        label { display:block; margin:15px 0 5px;}
-        input[type=text], input[type=password] { width:300px; padding:5px; border:solid 1px #000;}
-        
-        button, .prev, .next { background-color:#b0232a; padding:5px 10px; color:#fff; text-decoration:none;}
-        button:hover, .prev:hover, .next:hover { background-color:#000; text-decoration:none;}
-
-        button { border: none; }
-
-        #controls { background: #eee; box-shadow: 0 0 16px #999; height: 30px; position: fixed; padding: 10px; top: 0; left: 0; width: 100%; z-index: 1 }
-        #controls h1 { color: #666; display: inline-block; margin: 0 0 8px 0 }
-        #controls input[type=text] { border-color: #999; margin: 0 25px; width: 120px }
-        
-        #steps { margin: 80px 0 0 0 }
-    </style>
-    
     <link rel="stylesheet" href="{{asset('easyui/validationEngine.jquery.min.css')}}" />
-    <style>
-        .prev{float:left}
-        .next{float:right}
-        .steps{list-style:none;width:100%;overflow:hidden;margin:0;padding:0}
-        .steps li{color:#b0b1b3;font-size:24px;float:left;padding:10px;transition:all .3s;-moz-transition:all .3s;-webkit-transition:all .3s;-o-transition:all .3s}
-        .steps li span{font-size:11px;display:block}
-        .steps li.current{color:#000}
-        .breadcrumb{height:37px}
-        .breadcrumb li{background:#eee;font-size:14px}
-        .breadcrumb li.current:after{border-top:18px solid transparent;border-bottom:18px solid transparent;border-left:6px solid #666;content:' ';position:absolute;top:0;right:-6px}
-        .breadcrumb li.current{background:#666;color:#eee;position:relative}
-        .breadcrumb li:last-child:after{border:none}
-    </style>
-
-
+    <link rel="stylesheet" href="{{asset('css/wizard.css')}}" />
+    
 @endsection
-
 
 @section('content')
 <div id="cc" class="easyui-layout" style="width:100%;height:645px; margin-top: +10px">
 
     <div id="addacc" data-options="region:'east',split:true,collapsed:true" title="Account" style="width:450px; padding: 20px;">
-        
-
         <form id="accform" method="POST" action="{{url('mmmuser/account')}}">
-        <fieldset>
-            <label for="Name">Bitcoin Address</label>
-            <input id="Name" name="ac_number" type="text" class="validate[required]" />
-        </fieldset>
-        <fieldset>
-            <label for="ac_name">Account Name :</label>
-            <input id="ac_name" type="text" name="ac_name" class="validate[required]" />
-            <label for="bank_name">Bank Name :</label>
-            <input id="bank_name" type="text" name="bank_name" disabled="" value="Bitcoin" />
+            <fieldset class="wfieldset">
+                <label for="Name">Bitcoin Address</label>
+                <input id="Name" name="ac_number" type="text" class="validate[required]" />
+            </fieldset>
+            <fieldset class="wfieldset">
+                <label for="ac_name">Account Name :</label>
+                <input id="ac_name" type="text" name="ac_name" class="validate[required]" />
+                <label for="bank_name">Bank Name :</label>
+                <input id="bank_name" type="text" name="bank_name" disabled="" value="Bitcoin" />
 
-        </fieldset>
-        <fieldset>
-            <label for="ac_holder">Account Holder :</label>
-            <input id="ac_holder" type="text" name="ac_holder" class="validate[required]" />
-            <label for="ac_type">Account Type :</label>
-            <input id="ac_type" disabled="" type="text" name="ac_type" />
-            <label for="ac_currency">Account Currency</label>
-            <select id="ac_currency" name="ac_currency">
-                <option value="MAVRO-USD">MAVRO-USD</option>
-            </select>     
-            <label for="beneficiary_name">Beneficiary Name :</label>
-            <input id="beneficiary_name" type="text" name="beneficiary_name" />
-            {{Form::token()}}
-        </fieldset>
-        <p>
-            <input id="SaveAccount" onclick="saveUser()" type="button" value="Save Account" />
-        </p>
-    </form>
+            </fieldset>
+            <fieldset class="wfieldset">
+                <label for="ac_holder">Account Holder :</label>
+                <input id="ac_holder" type="text" name="ac_holder" class="validate[required]" />
+                <label for="ac_type">Account Type :</label>
+                <input id="ac_type" disabled="" type="text" name="ac_type" />
+                <label for="ac_currency">Account Currency</label>
+                <select id="ac_currency" name="ac_currency">
+                    <option value="MAVRO-USD">MAVRO-USD</option>
+                </select>     
+                <label for="beneficiary_name">Beneficiary Name :</label>
+                <input id="beneficiary_name" class="validate[required]" type="text" name="beneficiary_name" />
+                {{Form::token()}}
+            </fieldset>
+            <p>
+                <input id="SaveAccount" onclick="saveUser()" type="button" value="Save Account" />
+            </p>
+        </form>
     </div>
     <div data-options="region:'center',title:'Accounts'"  >
         <div class="easyui-layout" data-options="fit:true">
@@ -106,24 +71,16 @@
                     </tr>
                 </thead>
             </table>
-
-
         </div>
     </div>
 </div>
-    {{-- <div>
-        <div id="w" class="easyui-dialog" title="Warning" data-options="modal:true,closed:true" style="width:500px;height:200px;padding:10px;">
-            Are you sure, you Want to delete this?
-        </div>
-    </div> --}}
-
-
 
 @endsection
 
 
 
 @section('scripts')
+
 
 <script src="{{asset('easyui/jquery.validationEngine.min.js') }}"></script>
 <script src="{{asset('easyui/jquery.validationEngine-en.min.js')}}"></script>
