bb1520ce6d18291b51fa21692e5b13688594e2c9
siddas27 <siddharthdas8@gmail.com>
2017-03-02 18:26:24 +0530

    admin

--- resources/views/admin/assignments.blade.php
+++ resources/views/admin/assignments.blade.php
@@ -13,7 +13,28 @@
 
 
 @section('content')
-
+    <div data-options="region:'center',title:'Accounts'"  >
+        <div class="easyui-layout" data-options="fit:true">
+            {{Form::token()}}
+            <table id="dg" class="easyui-datagrid" style="width:100%;height: 580px;"
+                   url="{{ url('mmmuser/assignments') }}" method="get"
+                   toolbar="#toolbar" pagination="true"
+                   rownumbers="true" fitColumns="true" singleSelect="true">
+                <thead>
+                <tr>
+                    <th field="assignmentid" width="50">Assignment ID</th>
+                    <th field="ptname" width="50">Participant Name</th>
+                    <th field="amount" width="50">Amount</th>
+                    <th field="balance" width="50">Balance</th>
+                    <th field="msg" width="100">Message </th>
+                    <th field="ac_holderid" width="50">Card/Account Holder ID</th>
+                    <th field="created_at" width="50">Created At </th>
+                    <th field="status" width="50">Status</th>
+                </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
 @endsection
 
 
--- resources/views/admin/orders.blade.php
+++ resources/views/admin/orders.blade.php
@@ -13,6 +13,28 @@
 
 
 @section('content')
+    <div data-options="region:'center',title:'Accounts'"  >
+        <div class="easyui-layout" data-options="fit:true">
+            {{Form::token()}}
+            <table id="dg" class="easyui-datagrid" style="width:100%;height: 580px;"
+                   url="{{ url('mmmuser/orders') }}" method="get"
+                   toolbar="#toolbar" pagination="true"
+                   rownumbers="true" fitColumns="true" singleSelect="true">
+                <thead>
+                <tr>
+                    <th field="assignmentid" width="50">Assignment ID</th>
+                    <th field="orderid" width="50">Order ID</th>
+                    <th field="rptname" width="50">Recipient Name</th>
+                    <th field="amount" width="50">Amount</th>
+                    <th field="snrname" width="50">Sender Name</th>
+                    <th field="msg" width="100">Message </th>
+                    <th field="created_at" width="50">Created At </th>
+                    <th field="status" width="50">Status</th>
+                </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
 
 @endsection
 
