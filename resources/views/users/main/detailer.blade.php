50a845dc5b7b2dae6a72ea85ef901b0b58df7a0d
siddas27 <siddharthdas8@gmail.com>
2017-03-03 17:06:47 +0530

    detailer

---
+++ resources/views/users/main/detailer.blade.php
@@ -0,0 +1,6 @@
+<div  class="easyui-panel p1" style="width:360px;height:190px;padding:10px;">
+    <table>
+        <tr><img src="{{ asset('easyui/themes/icons/next.png') }}"><img src="{{ asset('easyui/themes/icons/2.png') }}">Confirm funds reception</tr>
+        <tr><img src="{{ asset('easyui/themes/icons/next.png') }}"><img src="{{ asset('easyui/themes/icons/play.png') }}">Extend payment expectation by 24hrs</tr>
+    </table>
+</div>
