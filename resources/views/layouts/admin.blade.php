f23fcb1169bbd2dd6a8ce65fe515704ed60ffc81
siddas27 <siddharthdas8@gmail.com>
2017-03-05 18:07:52 +0530

    news

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -80,7 +80,7 @@
                 <li>
                     <a href="{{url('admin/news')}}">
                         <i class="ti-bell"></i>
-                        <p>Notifications</p>
+                        <p>News</p>
                     </a>
                 </li>
             </ul>
@@ -102,30 +102,9 @@
                 <div class="collapse navbar-collapse">
                     <ul class="nav navbar-nav navbar-right">
                         <li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <a href="{{url('logout')}}" class="dropdown-toggle">
                                 <i class="ti-panel"></i>
-								<p>Stats</p>
-                            </a>
-                        </li>
-                        <li class="dropdown">
-                              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                    <i class="ti-bell"></i>
-                                    <p class="notification">5</p>
-									<p>Notifications</p>
-									<b class="caret"></b>
-                              </a>
-                              <ul class="dropdown-menu">
-                                <li><a href="#">Notification 1</a></li>
-                                <li><a href="#">Notification 2</a></li>
-                                <li><a href="#">Notification 3</a></li>
-                                <li><a href="#">Notification 4</a></li>
-                                <li><a href="#">Another notification</a></li>
-                              </ul>
-                        </li>
-						<li>
-                            <a href="#">
-								<i class="ti-settings"></i>
-								<p>Settings</p>
+								<p>Logout</p>
                             </a>
                         </li>
                     </ul>
